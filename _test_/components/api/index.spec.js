66839267dca36cbf027c172051bc1e1eba0998cd
Heyff12 <645068564@qq.com>
2022-05-14 20:10:57 +0800

    add api test

---
+++ _test_/components/api/index.spec.js
@@ -0,0 +1,21 @@
+import api from "../../../src/api";
+
+describe("Api test", () => {
+  describe("getAuctionList", () => {
+    test("return 3 auction items", async () => {
+      const data = await api.getAuctionList();
+
+      expect(data.length).toBe(3);
+    });
+
+    test("return error", async () => {
+      const fn = jest.fn();
+      try {
+        await api.getAuctionList();
+      } catch (err) {
+        fn();
+      }
+      expect(fn).toHaveBeenCalled();
+    });
+  });
+});
